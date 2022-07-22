e231b0890e1c508bf9cd3f71d46456837eacd18d
Dnatureguy <73432326+Dnatureguy@users.noreply.github.com>
2022-07-22 09:42:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const searchInput = document.querySelector("#search-input");
+
+searchInput.addEventListener("keydown", function(event) {
+    if(event.code === "Enter") {
+        search();
+    }
+});
+
+function search() {
+    const input = searchInput.Value;
+
+    window.location.href = "https://www.google.com/search?q=" + input + "&rlz=1C5CHFA_enNZ948NZ948&oq=" + input + "&aqs=chrome.0.69i59j69i57j0i271l3j69i60l3.1122j0j9&sourceid=chrome&ie=UTF-8"
+}
